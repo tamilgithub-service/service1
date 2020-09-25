2fd8dafed0a48958066de47a10a0f74fb94cf23a
tamilgithub-service <tamil5ag@gmail.com>
2020-09-25 20:24:06 +0530

    initial commit

---
+++ webappapi/src/main/java/azure/wenb/webappapi/WebappapiApplication.java
@@ -0,0 +1,13 @@
+package azure.wenb.webappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappapiApplication.class, args);
+	}
+
+}
